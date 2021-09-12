281080f705f335ee235bd9c51dc28901c74867b5
kumosekine <toshiaki_ganbare@hotmail.com>
2021-09-12 20:14:31 +0900

    itemsデータベースの設定とitem/modelesのバリデーション設定

---
+++ app/models/item.rb
@@ -0,0 +1,27 @@
+class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :delivery_fee
+  belongs_to :prefecture
+  belongs_to :delivery_day
+
+  with_options presence: true
+  validates :image, 
+  validates :product_name, length: { maximum:40 }
+  validates :description, length: { maximum:1000 }
+  end
+
+  with_options numericaleity: { other_than: 1 , message: "can't be blank"}
+  validates :category
+  validates :status
+  validates :delivery_fee
+  validates :prefecture
+  validates :delivery_day
+  end
+
+  validates :price, numericality: { only_integer: true, greater_than:0, less_than:9999999 }
+  
+  has_one_attached :image
+
+end
