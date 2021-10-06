572eb52f7b4a92976f8343b0100a987e7de7b6b0
kumosekine <toshiaki_ganbare@hotmail.com>
2021-10-06 20:58:27 +0900

    odrerモデルにバリデーションを追加。orderモデルのテストコード異常系に４つを追加

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,8 +6,10 @@ class Order
   validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
   validates :city
   validates :address
-  validates :tel, format: {with: /\A[0-9]+\z/, message: "is invalid. Please input half-width characters"}
+  validates :tel, format: {with: /\A[0-9]+\z/, message: "is invalid. Please input half-width characters"},length: { minimum:10 ,maximum: 11 }
   validates :token
+  validates :user_id
+  validates :item_id
   end
 
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -3,11 +3,15 @@ require 'rails_helper'
 RSpec.describe Order, type: :model do
   describe '商品購入情報の保存' do
    before do
-    @order = FactoryBot.build(:order)
+    @user = FactoryBot.create(:user)
+    @item = FactoryBot.create(:item)
+    @order = FactoryBot.build(:order, user_id: @user.id, item_id: @item.id)
+    sleep 0.1
    end
 
    context '内容に問題がない場合' do
     it 'すべての値が正しく入力されていば購入できること' do
+      # binding.pry
       expect(@order).to be_valid
     end
     it 'building_nameは空でも購入できる' do
@@ -52,11 +56,32 @@ RSpec.describe Order, type: :model do
       @order.valid?
       expect(@order.errors.full_messages).to include("Tel can't be blank")
     end
-    it 'telが半角数字以外では登録できない' do
+    it 'telが半角数字以外では購入できない' do
       @order.tel = '０１２３４'
       @order.valid?
       expect(@order.errors.full_messages).to include("Tel is invalid. Please input half-width characters")
     end
+    it '電話番号が9桁以下では購入できない' do
+      @order.tel = '123456789'
+      @order.valid?
+      expect(@order.errors.full_messages).to include("Tel is too short (minimum is 10 characters)")
+
+    end
+    it '電話番号が12桁以上では購入できない' do
+      @order.tel = '123456789012'
+      @order.valid?
+      expect(@order.errors.full_messages).to include("Tel is too long (maximum is 11 characters)")
+    end
+    it 'userが紐付いていなければ購入できない' do
+      @order.user_id = nil
+      @order.valid?
+      expect(@order.errors.full_messages).to include("User can't be blank")
+    end
+     it 'itemが紐付いていなければ購入できない' do
+      @order.item_id = nil
+      @order.valid?
+      expect(@order.errors.full_messages).to include("Item can't be blank")
+     end
    end
   end
 end
